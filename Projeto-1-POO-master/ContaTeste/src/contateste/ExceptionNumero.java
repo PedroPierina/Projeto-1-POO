7c330ca5624cd89ed4579ff4fcefbdcdbfe0ba56
brunokitaka <brunokitaka@gmail.com>
2017-10-02 14:43:49 -0300

    Add files via upload

---
+++ Projeto-1-POO-master/ContaTeste/src/contateste/ExceptionNumero.java
@@ -0,0 +1,11 @@
+package contateste;
+import java.util.InputMismatchException;
+
+public class ExceptionNumero {
+    
+    public static int quotient(int numerador, int divisor) throws ArithmeticException {
+        return numerador/divisor;
+    }
+    
+            
+}
